5683e1e08488dedab3a540154c961fd4752b46dc
flor09 <49553115@est.ort.edu.ar>
2026-03-15 21:06:01 -0300

    agregado

--- script.js
+++ script.js
@@ -88,3 +88,130 @@ function contarLetras(texto)
             }
             console.log(numeroMayor);
         }
+           function obtenerPares(numeros)
+            {
+                let numerosPares = [];
+
+                for(let i = 0; i < numeros.length; i++)
+                {
+                    if(numeros[i] % 2 === 0)
+                    {
+                        numerosPares.push(numeros[i]);
+                    }
+                }
+                return numerosPares;
+            }
+   //PARTE3
+         const usuario = {nombre: "Ana", edad: 20, activo:false}
+        
+
+         function descripcionUsuario(usuario)
+        {
+            return usuario.nombre+ "tiene" +usuario.edad+ "años" ;
+
+        }
+        console.log (descripcionUsuario(usuario));
+
+
+        function ej9activarUsuario(usuario)
+        {
+            usuario.activo = true;
+            return usuario.activo;
+        }
+                const productos = [
+                {nombre: "Mouse", precio:10},
+                {nombre: "Teclado", precio:25},
+                {nombre: "Monitor", precio:200}
+                ] 
+        function ej10PrecioTotal(productos)
+        {
+            let total=0;
+            for(let i=0; i<productos.length ;i++)
+            {
+                total+= productos[i].precio;
+            }
+            return total;
+
+        }
+        console.log(ej10PrecioTotal(productos));
+        //ej11
+                const usuarios = [
+                {nombre:"Ana", edad:17},
+                {nombre:"Juan", edad:25},
+                {nombre:"Pedro", edad:30}
+                ];
+                const nombres = usuarios.map(usuario => usuario.nombre);
+
+                console.log(nombres);
+        //ej 12
+        const mayores = usuarios.filter(usuario => usuario.edad >= 18);
+
+        console.log(mayores);
+
+
+        //ej 13
+        const totalEdades = usuarios.reduce((acum, usuario) => acum + usuario.edad, 0);
+
+        console.log(totalEdades);
+        //ej14
+        const producto = {
+        nombre:"Notebook",
+        precio:1000 }
+        function mostrarUsuario(nombre, precio)
+        {
+            Console.log("Nombre: " + nombre);
+            Console.log( "Precio: "+ precio);
+        }
+        //ej15
+        const productoStock = {... producto, stock:5  }
+        function agregarStock(productoStock)
+        {
+            return productoStock;
+        }
+        //ej16
+         function buscarProducto(productos, nombre) {
+         return productos.find(p => p.nombre === nombre);
+         }  
+        // ej17
+        function productosMayoresA50(productos) {
+         return productos.filter(producto => producto.precio > 50);
+         } 
+         //ej18  
+         function promedio(numeros) {
+        const suma = numeros.reduce((acum, num) => acum + num, 0);
+        return suma / numeros.length;
+        }
+
+        //desafio final API
+        const Usuarios = [
+        {id:1, nombre:"Ana", edad:20},
+        {id:2, nombre:"Juan", edad:15},
+        {id:3, nombre:"Pedro", edad:30}
+        ];
+        //ej1
+        function obtenerUsuarios() {
+            return usuarios;
+        }
+
+        console.log(obtenerUsuarios());
+        //ej2
+        function obtenerUsuarioPorId(id) {
+        return usuarios.find(usuario => usuario.id === id);
+        }
+        //ej3
+        function obtenerMayores() {
+        return usuarios.filter(usuario => usuario.edad >= 18);
+        }
+
+        //ej4
+        function crearUsuario(nombre, edad) {
+        const nuevoUsuario = {
+        id: usuarios.length + 1,
+        nombre: nombre,
+        edad: edad
+        };
+
+        usuarios.push(nuevoUsuario);
+        return nuevoUsuario;
+        }
+        
